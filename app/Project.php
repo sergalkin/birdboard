0e2c0c3dfc04989c7ac739f6af204218db15a04f
sergalkin <sa.galkin94@gmail.com>
2019-03-17 12:10:31 +0300

    basic crud

---
+++ app/Project.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Project
+ *
+ * @property int $id
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Project newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Project newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Project query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Project whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Project whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Project whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Project whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Project extends Model
+{
+    protected $guarded = [];
+
+    public function path()
+    {
+        return "/projects/$this->id";
+    }
+}
---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<h1>Birdboard</h1>
+<ul>
+    @forelse($projects as $project)
+        <li>
+            <a href="{{ $project->path() }}">
+                {{ $project->title }}
+            </a>
+        </li>
+    @empty
+        <li>No Projects yet.</li>
+    @endforelse
+</ul>
+</body>
+</html>
